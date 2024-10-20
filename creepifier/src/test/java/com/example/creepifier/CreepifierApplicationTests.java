e93bb5114191cb52260d04d81de9c50f81b539c1
Zach Glossip <zglossip@gmail.com>
2024-10-20 12:35:32 -0500

    init

---
+++ creepifier/src/test/java/com/example/creepifier/CreepifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.creepifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreepifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
